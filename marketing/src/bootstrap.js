b9659ceaae85b016634a881d9d354bcaa9c57686
JatinBumbra <jatin.bumbra.23@gmail.com>
2021-12-21 16:55:38 +0530

    Setup navigation exchange from container to marketing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} />, el);
 };
 
 // If we are in dev and in isolation, call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const el = document.getElementById('_marketing-dev-root');
-  if (el) mount(el);
+  if (el) mount(el, { defaultHistory: createBrowserHistory() });
 }
 
 // We are running and container and export the mount function
